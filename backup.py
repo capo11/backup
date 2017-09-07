919a54d56c397ef567f4a84fe634b645c771062c
Andrea Capobianco <andreacapobianco29@gmail.com>
2017-09-07 13:34:38 +0100

    beta2

--- backup.py
+++ backup.py
@@ -3,29 +3,41 @@ import shutil
 from datetime import *
 import tarfile
 
-#step 0
+# step 0
 if not (os.path.exists('backup')):
-	os.mkdir('backup')
+    os.mkdir('backup')
 
-#step 1
+# step 1
 today = datetime.today()
 week = today.strftime("%U")
 os.mkdir('./backup/W' + week)
-#step 2
+# step 2
 listadir = os.listdir('./')
 for linea in listadir:
-	linea.strip()
-	if not (linea == 'backup' or linea == 'backup.py'):
-		if week in linea:
-			shutil.move('./' + linea, './backup/W' + week)
-#step 3
+    linea.strip()
+    if not (linea == 'backup' or linea == 'backup.py'):
+        if week in linea:
+            shutil.move('./' + linea, './backup/W' + week)
+# step 3
 with tarfile.open('./backup/W' + week + '.tgz', 'w:gz') as tar:
-	tar.add('./backup/W' + week, arcname=os.path.basename('./backup/W' + week))
-#step 4
+    tar.add('./backup/W' + week, arcname=os.path.basename('./backup/W' + week))
+# step 4
 if not(os.path.exists('trash')):
-	os.mkdir('trash')
-#step 5
+    os.mkdir('trash')
+# step 5
 tutteDirectory = os.listdir('./')
 for linea in tutteDirectory:
-	if not (linea == "backup" or linea == "backup.py"):
-		shutil.move('./' + linea, './trash')
+    if not (linea == "backup" or linea == "backup.py" or linea == "rootDati.log"):
+        shutil.move('./' + linea, './trash')
+# step 6
+log = open('rootDati.log', 'w')
+contaBackup = 0
+contaTrash = 0
+tutteDirectory = os.listdir('./backup')
+for linea in tutteDirectory:
+    contaBackup += 1
+tutteDirectory = os.listdir('./trash')
+for linea in tutteDirectory:
+    contaTrash += 1
+log.write('w' + week + ': ' + str(contaBackup) + ':' + str(contaTrash))
+log.close()
