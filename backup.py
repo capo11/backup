7586bbd552fdb09cb985c8f189ccce2a5885fec7
unknown <lorenzopisa00@gmail.com>
2017-09-07 10:36:59 +0100

    sono un pro, step2

--- backup.py
+++ backup.py
@@ -1,5 +1,10 @@
 import os.path
 import shutil
+from datetime import *
 
 if not (os.path.exists('backup')):
-	os.mkdir('backup')
+    os.mkdir('backup')
+
+today = datetime.today()
+week = today.strftime("%U")
+os.mkdir('./backup/W' + week)
