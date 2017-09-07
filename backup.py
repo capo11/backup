0e7a28360e9e959398869a37c11837b846b70d4c
unknown <lorenzopisa00@gmail.com>
2017-09-07 10:45:58 +0100

    step0,step1,step4,step5

--- backup.py
+++ backup.py
@@ -5,6 +5,12 @@ from datetime import *
 if not (os.path.exists('backup')):
     os.mkdir('backup')
 
+if not(os.path.exist('trash')):
+    os.mkdir('trash')
+tutteDirectory = os.listdir('./')
+for linea in tutteDirectory():
+    if not (linea == "backup" or linea == "backup.py"):
+        shutil.move('./' + linea, './trash')
 today = datetime.today()
 week = today.strftime("%U")
 os.mkdir('./backup/W' + week)
